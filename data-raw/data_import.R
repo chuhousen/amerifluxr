ad417dba498f74569bc2a50fd450ead45d820864
khufkens <koen.hufkens@gmail.com>
2021-11-06 18:20:23 +0100

    xz compression is smaller than the default

--- data-raw/data_import.R
+++ data-raw/data_import.R
@@ -5,5 +5,5 @@ amf_bif <- readxl::read_xlsx(file.path(getwd(),"data-original\\","AMF_AA-Flx_BIF
 amf_base <- utils::read.table(unz(paste0(getwd(), "\\data-original\\", "AMF_US-CRT_BASE-BADM_4-5.zip"), "AMF_US-CRT_BASE_HH_4-5.csv"),
                               sep=",", na=c("-9999"), header=T, skip=2)
 
-usethis::use_data(amf_bif, amf_bif)
-usethis::use_data(amf_base, amf_base)
+usethis::use_data(amf_bif, amf_bif, compress = "xz")
+usethis::use_data(amf_base, amf_base, compress = "xz")
